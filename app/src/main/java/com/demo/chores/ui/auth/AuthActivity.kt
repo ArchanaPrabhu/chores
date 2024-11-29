7fd80646bbe7c667382e5640173ed983ea806654
Archana Prabhu <archanaprabhu@Archanas-MacBook-Pro.local>
2024-11-28 22:06:08 -0800

    Build with auth login setup

---
+++ app/src/main/java/com/demo/chores/ui/auth/AuthActivity.kt
@@ -0,0 +1,14 @@
+package com.demo.chores.ui.auth
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.demo.chores.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
